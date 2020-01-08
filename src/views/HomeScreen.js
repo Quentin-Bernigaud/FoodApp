530fbfb6c50ab42872619b5f1fa50a495ee03b27
Quentin <quentin@bernigaud.fr>
2020-01-08 15:32:53 +0100

    Clean base

---
+++ src/views/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Page d'accueil</Text>
+            </View>
+        );
+    }
+
+    static navigationOptions = {
+        tabBarLabel: 'Home!',
+      };
+}
+
---
+++ src/views/ScanScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class ScanScreen extends React.Component {
+    render() {
+        return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Page de scan</Text>
+            </View>
+        );
+    }
+}
