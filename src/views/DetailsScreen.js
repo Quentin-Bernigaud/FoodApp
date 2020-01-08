972bc49c06484f98f377524da63ad6678f139477
Quentin <quentin@bernigaud.fr>
2020-01-08 18:33:27 +0100

    Add Home list content API

--- src/views/DetailsScreen.js
+++ src/views/DetailsScreen.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  TouchableOpacity
+} from "react-native";
+import { Icon } from "native-base";
 
 export default class DetailsScreen extends React.Component {
-    render() {
-        return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Details!</Text>
-            </View>
-        );
-    }
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: () => (
+        <Text>{navigation.getParam("product_name", "product name")}</Text>
+      ),
+      headerRight: () => (
+        <TouchableOpacity onPress={() => alert("This is a button!")}>
+          <Icon name="heart" type="EvilIcons" />
+        </TouchableOpacity>
+      )
+    };
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1, paddingTop: 20 }}>
+        <Text>{this.props.navigation.state.params.product_name}</Text>
+        <Image
+          source={{
+            uri: this.props.navigation.state.params.item.image_small_url
+          }}
+          style={{ width: 400, height: 400 }}
+        />
+      </SafeAreaView>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  lineContainer: {
+    height: 40,
+    padding: 10
+  }
+});
--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,16 +1,88 @@
 import React from "react";
-import { View, Text } from "react-native";
 
-export default class HomeScreen extends React.Component {
+import {
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  Image
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+// import { Audio } from "expo-av";
+
+class ListItem extends React.Component {
+  _onPress(item) {
+    this.props.navigation.navigate("Details", {
+      product_name: item.product_name,
+      item: item
+    });
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Page d'accueil</Text>
+      <View style={styles.lineContainer}>
+        <TouchableOpacity onPress={() => this._onPress(this.props.item)}>
+          <Text>{this.props.item.product_name}</Text>
+        </TouchableOpacity>
       </View>
     );
   }
-
-  static navigationOptions = {
-    tabBarLabel: "Home"
-  };
 }
+
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    return fetch("https://fr-en.openfoodfacts.org/category/pizzas/1.json")
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson.products
+          },
+          function() {}
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      <SafeAreaView style={{ flex: 1, paddingTop: 20 }}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => (
+            <ListItem item={item} navigation={this.props.navigation} />
+          )}
+          keyExtractor={({ id }, index) => id}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  lineContainer: {
+    paddingLeft: 10,
+    paddingBottom: 5
+  }
+});
