64fadd163c3ab96d700d8a019d1ac790987fd85a
Quentin <quentin@bernigaud.fr>
2020-01-10 15:47:56 +0100

    Reset Scan to initial way

--- src/views/HistoryScreen.js
+++ src/views/HistoryScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default class ScanScreen extends React.Component {
+export default class HistoryScreen extends React.Component {
     render() {
         return (
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
--- src/views/ScanScreen.js
+++ src/views/ScanScreen.js
@@ -1,84 +1,97 @@
-import React, { PureComponent } from 'react';
-import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-const PendingView = () => (
-  <View
-    style={{
-      flex: 1,
-      backgroundColor: 'lightgreen',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <Text>Waiting</Text>
-  </View>
-);
-
-class ScanScreen extends PureComponent {
-  render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-        >
-          {({ camera, status, recordAudioPermissionStatus }) => {
-            if (status !== 'READY') return <PendingView />;
-            return (
-              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.capture}>
-                  <Text style={{ fontSize: 14 }}> SNAP </Text>
-                </TouchableOpacity>
-              </View>
-            );
-          }}
-        </RNCamera>
-      </View>
-    );
-  }
-
-  takePicture = async function(camera) {
-    const options = { quality: 0.5, base64: true };
-    const data = await camera.takePictureAsync(options);
-    //  eslint-disable-next-line
-    console.log(data.uri);
-  };
+export default class ScanScreen extends React.Component {
+    render() {
+        return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Page de scan</Text>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-  },
-});
+// import React, { PureComponent } from 'react';
+// import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+// import { RNCamera } from 'react-native-camera';
 
-AppRegistry.registerComponent('App', () => ScanScreen);
+// const PendingView = () => (
+//   <View
+//     style={{
+//       flex: 1,
+//       backgroundColor: 'lightgreen',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//     }}
+//   >
+//     <Text>Waiting</Text>
+//   </View>
+// );
+
+// class ScanScreen extends PureComponent {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <RNCamera
+//           style={styles.preview}
+//           type={RNCamera.Constants.Type.back}
+//           flashMode={RNCamera.Constants.FlashMode.on}
+//           androidCameraPermissionOptions={{
+//             title: 'Permission to use camera',
+//             message: 'We need your permission to use your camera',
+//             buttonPositive: 'Ok',
+//             buttonNegative: 'Cancel',
+//           }}
+//           androidRecordAudioPermissionOptions={{
+//             title: 'Permission to use audio recording',
+//             message: 'We need your permission to use your audio',
+//             buttonPositive: 'Ok',
+//             buttonNegative: 'Cancel',
+//           }}
+//         >
+//           {({ camera, status, recordAudioPermissionStatus }) => {
+//             if (status !== 'READY') return <PendingView />;
+//             return (
+//               <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+//                 <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.capture}>
+//                   <Text style={{ fontSize: 14 }}> SNAP </Text>
+//                 </TouchableOpacity>
+//               </View>
+//             );
+//           }}
+//         </RNCamera>
+//       </View>
+//     );
+//   }
+
+//   takePicture = async function(camera) {
+//     const options = { quality: 0.5, base64: true };
+//     const data = await camera.takePictureAsync(options);
+//     //  eslint-disable-next-line
+//     console.log(data.uri);
+//   };
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     backgroundColor: 'black',
+//   },
+//   preview: {
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   capture: {
+//     flex: 0,
+//     backgroundColor: '#fff',
+//     borderRadius: 5,
+//     padding: 15,
+//     paddingHorizontal: 20,
+//     alignSelf: 'center',
+//     margin: 20,
+//   },
+// });
+
+// AppRegistry.registerComponent('App', () => ScanScreen);
